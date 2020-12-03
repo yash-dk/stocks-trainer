db89d86ef7b5cc31c09405c8238f0337c3bab585
yash-dk <yashkhadse@gmail.com>
2020-12-03 19:57:21 +0530

    added comments and changed var names

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ class App(tk.Frame):
         self.construct_widget()
 
     def fonts(self):
+        """
+            Create standard font sizes.
+        """
         self.font_normal = tkFont.Font(
             family="Times New Roman",
             weight="bold", size=20)
@@ -28,6 +31,9 @@ class App(tk.Frame):
             size=round(20 * .66))
 
     def construct_widget(self):
+        # Main GUI
+
+        # Symbol
         tk.Label(self,
                  text="BITCOIN",
                  font=self.font_normal,
@@ -35,18 +41,21 @@ class App(tk.Frame):
                  ).grid(row=0, column=0, columnspan=4, sticky="nsew",
                         padx=5, pady=5)
 
+        # Symbol Full Name
         tk.Label(self,
                  text="BITCOIN PTV. LTD."
                  ).grid(row=1, column=0, columnspan=4, sticky="nsew")
 
         ttk.Separator(self).grid(row=2, column=0, columnspan=4, sticky="nsew")
 
+        # Buy Button
         tk.Button(self,
                   text="BUY\n$2020",
                   font=self.font_small
                   ).grid(row=3, column=0, columnspan=2, sticky="nsew",
                          padx=5, pady=5)
 
+        # Sell Button
         tk.Button(self,
                   text="SELL\n$2020",
                   font=self.font_small
@@ -56,18 +65,21 @@ class App(tk.Frame):
         ttk.Separator(self
                       ).grid(row=4, column=0, columnspan=4, sticky="nsew")
 
+        # Initialize replay
         tk.Button(self,
                   text="◀◀",
                   font=self.font_small
                   ).grid(row=5, column=0, columnspan=4, sticky="nsew",
                          padx=5, pady=5)
 
+        # Play / Pause the replay
         tk.Button(self,
                   text="▶",
                   font=self.font_small
                   ).grid(row=6, column=0, columnspan=2, sticky="nsew",
                          padx=5, pady=5)
 
+        # Advance the bar by one
         tk.Button(self,
                   text="⏯",
                   font=self.font_small
@@ -77,6 +89,7 @@ class App(tk.Frame):
         ttk.Separator(self
                       ).grid(row=7, column=0, columnspan=4, sticky="nsew")
 
+        # OHLC Data of surrent candle
         ttk.Label(self,
                   text="open\n15.12"
                   ).grid(row=8, column=0, columnspan=1, sticky="nsew")
@@ -95,86 +108,96 @@ class App(tk.Frame):
 
         ttk.Separator(self).grid(row=9, column=0, columnspan=4, sticky="nsew")
 
-        tabscon = ttk.Notebook(self)
-        tab1 = ttk.Frame(tabscon)
-        tab2 = ttk.Frame(tabscon)
-        tabscon.add(tab1, text="Market")
-        tabscon.add(tab2, text="Limit")
-        tabscon.grid(row=10, column=0, columnspan=4, sticky="nsew")
-        tab1.grid_columnconfigure(1, weight=1)
-        tk.Label(tab1,
+        # Limit order and Market Order Tabs
+        orderstab = ttk.Notebook(self)
+
+        # Market Tab
+        markettab = ttk.Frame(orderstab)
+        # Limit Tab
+        limittab = ttk.Frame(orderstab)
+
+        orderstab.add(markettab, text="Market")
+        orderstab.add(limittab, text="Limit")
+
+        # Add tabs to main GUI
+        orderstab.grid(row=10, column=0, columnspan=4, sticky="nsew")
+
+        # Configure Market Tab
+        markettab.grid_columnconfigure(1, weight=1)
+        tk.Label(markettab,
                  text="MARKET ORDER",
                  font=self.font_small
                  ).grid(row=0, column=0,  columnspan=4, sticky="nsew",
                         padx=5, pady=5)
 
-        tk.Label(tab1,
+        tk.Label(markettab,
                  text="Quantity"
                  ).grid(row=1, column=0,  columnspan=1, sticky="nsew",
                         padx=5, pady=5)
 
-        tk.Entry(tab1
+        tk.Entry(markettab
                  ).grid(row=1, column=1,  columnspan=3, sticky="nsew",
                         padx=5, pady=5)
 
-        tk.Label(tab1,
+        tk.Label(markettab,
                  text="Take Profit"
                  ).grid(row=2, column=0,  columnspan=1, sticky="nsew",
                         padx=5, pady=5)
 
-        tk.Entry(tab1
+        tk.Entry(markettab
                  ).grid(row=2, column=1,  columnspan=3, sticky="nsew",
                         padx=5, pady=5)
 
-        tk.Label(tab1,
+        tk.Label(markettab,
                  text="Stop Loss"
                  ).grid(row=3, column=0,  columnspan=1, sticky="nsew",
                         padx=5, pady=5)
 
-        tk.Entry(tab1
+        tk.Entry(markettab
                  ).grid(row=3, column=1,  columnspan=3, sticky="nsew",
                         padx=5, pady=5)
 
-        tab2.grid_columnconfigure(1, weight=1)
-        tk.Label(tab2,
+        # Configure Limit Tab
+        limittab.grid_columnconfigure(1, weight=1)
+        tk.Label(limittab,
                  text="LIMIT ORDER",
                  font=self.font_small
                  ).grid(row=0, column=0,  columnspan=4, sticky="nsew",
                         padx=5, pady=5)
 
-        tk.Label(tab2,
+        tk.Label(limittab,
                  text="Order Price"
                  ).grid(row=1, column=0,  columnspan=1, sticky="nsew",
                         padx=5, pady=5)
 
-        tk.Entry(tab2
+        tk.Entry(limittab
                  ).grid(row=1, column=1,  columnspan=3, sticky="nsew",
                         padx=5, pady=5)
 
-        tk.Label(tab2,
+        tk.Label(limittab,
                  text="Quantity"
                  ).grid(row=2, column=0,  columnspan=1, sticky="nsew",
                         padx=5, pady=5)
 
-        tk.Entry(tab2
+        tk.Entry(limittab
                  ).grid(row=2, column=1,  columnspan=3, sticky="nsew",
                         padx=5, pady=5)
 
-        tk.Label(tab2,
+        tk.Label(limittab,
                  text="Take Profit"
                  ).grid(row=3, column=0,  columnspan=1, sticky="nsew",
                         padx=5, pady=5)
 
-        tk.Entry(tab2
+        tk.Entry(limittab
                  ).grid(row=3, column=1,  columnspan=3, sticky="nsew",
                         padx=5, pady=5)
 
-        tk.Label(tab2,
+        tk.Label(limittab,
                  text="Stop Loss"
                  ).grid(row=4, column=0,  columnspan=1, sticky="nsew",
                         padx=5, pady=5)
 
-        tk.Entry(tab2
+        tk.Entry(limittab
                  ).grid(row=4, column=1,  columnspan=3, sticky="nsew",
                         padx=5, pady=5)
 
